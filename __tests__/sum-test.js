89412e2d00e8921904969c284feb65646536808a
Tomoo Morikawa <tomoo.morikawa.84@shiftinc.jp>
2020-12-17 00:30:27 +0900

    fix depulicatted description

--- __tests__/sum-test.js
+++ __tests__/sum-test.js
@@ -7,4 +7,9 @@ describe('sum', function() {
     var sum = require('../src/sum');
     expect(sum(1, 2)).toBe(3);
   });
+
+  it('adds 3 + 4 to equal 7', function() {
+    var sum = require('../src/sum');
+    expect(sum(3, 4)).toBe(7);
+  });
 });
